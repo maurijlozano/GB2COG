9a4a603d614125ac0fcf753ed3be6db4d857421f
Mauricio J. Lozano <maurijlozano@gmail.com>
2020-08-03 09:51:04 -0300

    Add files via upload
    Added evalue, pident and qcoverage options

--- GB2COG.py
+++ GB2COG.py
@@ -3,6 +3,7 @@
 GB2COG.py Extract Proteins from Genbank file.
 M.J.Lozano 2020
 '''
+import argparse
 import sys, os, re, subprocess,multiprocessing
 import pandas as pd
 from Bio import SeqIO
@@ -19,22 +20,53 @@ def COG2FUN(row):
 	fun=fun[0]
 	return fun
 
+def parseArgs():
+	'''
+	Argument parsing is done.
+	'''
+	parser = argparse.ArgumentParser(description='GB2COG extracts protein sequences and assigns COGs using rpsblast+..')
+	parser.add_argument("-q", "--QueryFiles",help="Query genomes in genbank format...", dest="qfiles", action='append', nargs='+', required=False)
+	parser.add_argument("-i", "--ID",help="Percent ID for rpsblast+...", dest="pid", action='store', required=False)
+	parser.add_argument("-e", "--evalue",help="Evalue for rpsblast+...", dest="evalue", action='store', required=False)
+	parser.add_argument("-c", "--queryCoverage",help="Query coverage for rpsblast+...", dest="qc", action='store', required=False)
+	args = parser.parse_args()
+	return args
 
 
-if len(sys.argv) < 2:
-	print("Arguments required: space separated Genebank files...")
+args = parseArgs()
+if not args.qfiles:
+	print("Error: you must provide space separated Genebank files...")
 	exit()
+
+if not args.pid:
+	pid = 0
+else:
+	pid = int(args.pid)
+
+if not args.qc:
+	qc = 0
+else:
+	qc = int(args.qc)
+	
+if not args.evalue:
+	evalue = 0.001
+else:	
+	evalue = args.evalue
 
 ncpu=multiprocessing.cpu_count()
 cdd2cog = pd.read_table('cdd2cog.txt',names=['CDD','COG','Gene','Description','',])
 cog2fun = pd.read_table('cog2fun.txt',names=['COG','func','name'])
 
-for filename in sys.argv[1:]:
+for filename in args.qfiles[0]:
 	print('Processsing '+str(filename)+'...')
 	basename = os.path.splitext(filename)[0]
 	i=1
+	allCOGs=pd.DataFrame()
 	for rec in SeqIO.parse(filename, "genbank"):
 		n = 1
+		description = rec.description
+		if len(description) == 0:
+			description = rec.name
 		faaName = basename+'_'+str(i)+'.faa'
 		faaCOG = basename+'_'+str(i)+'.cog'
 		f = open(faaName, '+w')
@@ -52,20 +84,34 @@ for filename in sys.argv[1:]:
 						f.write('>'+feature_name+'|'+feature_product+'|'+feature_protid+'\n'+str(feature_seq)+'\n')
 						n+=1
 		f.close()
-		subprocess.call(['rpsblast+','-query', faaName, '-db', 'Cog_LE/Cog', '-out', faaCOG, '-evalue', '0.00001',  '-num_threads', str(int(ncpu)), '-max_target_seqs' ,'1', '-outfmt', '6'])
-		blastRes = pd.read_table(faaCOG,names=['qseqid','sseqid','pident','length','mismatch','gapopen','qstart','qend','sstart','send','evalue','bitscore'])
+		subprocess.call(['rpsblast+','-query', faaName, '-db', 'Cog_LE/Cog', '-out', faaCOG, '-evalue', evalue,  '-num_threads', str(int(ncpu)), '-max_target_seqs' ,'1', '-outfmt', '6 qseqid sseqid pident qlen length evalue'])
+		blastRes = pd.read_table(faaCOG,names=['qseqid','sseqid','pident','qlen','length','evalue'])
+		blastRes = blastRes[blastRes['pident'] > pid]
+		blastRes['qc'] = blastRes['qlen']/blastRes['length']*100
+		blastRes = blastRes[blastRes['qc'] > qc]
 		blastRes = blastRes[['qseqid','sseqid']]
 		blastRes = blastRes.groupby('qseqid', as_index=False).first()
 		#
 		#search cdd on cdd2cog
-		blastRes['CDD'] = blastRes.apply(lambda x: re.sub(r'CDD:','',x["sseqid"]), axis=1)
+		blastRes['CDD'] = blastRes.apply(lambda x: re.sub(r'gnl\|CDD\|','',x["sseqid"]), axis=1)
 		blastRes['COG'] = blastRes.apply(lambda x: CDD2COG(x), axis=1)
 		blastRes['FUN'] = blastRes.apply(lambda x: COG2FUN(x), axis=1)
 		blastRes.to_csv(basename+'_'+str(i)+'_Full_Tab.csv')
 		found = len(blastRes)
 		COGdistrb = blastRes.groupby(['FUN'], as_index=False).aggregate({'COG':'count'})
-		COGdistrb = COGdistrb.append({'FUN':'Not Found','COG':n-found},ignore_index=True)
-		COGdistrb.to_csv(faaCOG)
+		completeCOGS = pd.DataFrame(columns=['FUN','COG'])
+		for k in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
+			if any(COGdistrb['FUN']==k):
+				completeCOGS = completeCOGS.append({'FUN':k,'COG':COGdistrb[COGdistrb['FUN']==k].iloc[0,1]},ignore_index=True)
+			else:
+				completeCOGS = completeCOGS.append({'FUN':k,'COG':0},ignore_index=True)
+		#		
+		completeCOGS = completeCOGS.append({'FUN':'Not Found','COG':n-found},ignore_index=True)
+		completeCOGS = completeCOGS.set_index('FUN')
+		completeCOGS = completeCOGS.rename(columns={'COG':description})
+		completeCOGS.to_csv(faaCOG)
+		#All in one table
+		allCOGs = pd.concat([allCOGs,completeCOGS],axis=1)
 		i+=1
-	
+	allCOGs.to_csv('COG_matrix.txt')
 
